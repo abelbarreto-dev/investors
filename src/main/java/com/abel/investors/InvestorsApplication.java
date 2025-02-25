2ba28b58f2a30bd60d7456bb81fa72cf85f1b4e2
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-02-25 12:20:54 -0300

    feat: project structure generated

---
+++ src/main/java/com/abel/investors/InvestorsApplication.java
@@ -0,0 +1,13 @@
+package com.abel.investors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestorsApplication.class, args);
+	}
+
+}
