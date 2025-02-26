ae5d0dac425fe7fec90433878bc09554e672befe
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-02-26 14:00:20 -0300

    feat: create service structure for account stock

---
+++ src/main/java/com/abel/investors/services/IServiceAccountStock.java
@@ -0,0 +1,14 @@
+package com.abel.investors.services;
+
+import com.abel.investors.models.AccountStock;
+import com.abel.investors.records.AccountStockRecord;
+import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
+
+public interface IServiceAccountStock {
+    public ResponseEntity<AccountStock> createAccountStock(AccountStockRecord accountStock);
+    public ResponseEntity<AccountStock> updateAccountStock(AccountStock accountStock, UUID id);
+    public ResponseEntity<AccountStock> deleteAccountStock(UUID id);
+    public ResponseEntity<AccountStock> getAccountStockByID(UUID id);
+}
---
+++ src/main/java/com/abel/investors/services/ServiceAccountStock.java
@@ -0,0 +1,35 @@
+package com.abel.investors.services;
+
+import com.abel.investors.models.AccountStock;
+import com.abel.investors.records.AccountStockRecord;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServiceAccountStock implements IServiceAccountStock {
+    @Override
+    public ResponseEntity<AccountStock> createAccountStock(AccountStockRecord accountStock) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<AccountStock> updateAccountStock(AccountStock accountStock, UUID id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<AccountStock> deleteAccountStock(UUID id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<AccountStock> getAccountStockByID(UUID id) {
+        return null;
+    }
+}
