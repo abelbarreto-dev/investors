cd2c42b8507ee39b7ae0fd8af07f419d803cdbe2
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-02-26 12:42:40 -0300

    feat: create service structure for billing address

---
+++ src/main/java/com/abel/investors/services/IServiceBillingAddress.java
@@ -0,0 +1,16 @@
+package com.abel.investors.services;
+
+import com.abel.investors.models.BillingAddress;
+import com.abel.investors.records.BillingAddressRecord;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface IServiceBillingAddress {
+    public ResponseEntity<BillingAddress> createBillingAddress(BillingAddressRecord billingAddress);
+    public ResponseEntity<BillingAddress> updateBillingAddress(BillingAddressRecord billingAddress);
+    public ResponseEntity<BillingAddress> deleteBillingAddressByID(UUID id);
+    public ResponseEntity<BillingAddress> getBillingAddressByID(UUID id);
+    public ResponseEntity<List<BillingAddress>> getBillingsAddressesByAccountID(UUID accountId);
+}
---
+++ src/main/java/com/abel/investors/services/ServiceBillingAddress.java
@@ -0,0 +1,41 @@
+package com.abel.investors.services;
+
+import com.abel.investors.models.BillingAddress;
+import com.abel.investors.records.BillingAddressRecord;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServiceBillingAddress implements IServiceBillingAddress {
+    @Override
+    public ResponseEntity<BillingAddress> createBillingAddress(BillingAddressRecord billingAddress) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<BillingAddress> updateBillingAddress(BillingAddressRecord billingAddress) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<BillingAddress> deleteBillingAddressByID(UUID id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<BillingAddress> getBillingAddressByID(UUID id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<BillingAddress>> getBillingsAddressesByAccountID(UUID accountId) {
+        return null;
+    }
+}
