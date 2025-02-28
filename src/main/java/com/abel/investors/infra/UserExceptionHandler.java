29245e3c0ae490e1a1cc1426ac3831c0ec5b5cc3
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-02-28 12:36:39 -0300

    feat: user exception handler controller advice

---
+++ src/main/java/com/abel/investors/infra/UserExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.abel.investors.infra;
+
+import com.abel.investors.exceptions.EmailException;
+import com.abel.investors.exceptions.PasswordException;
+import com.abel.investors.exceptions.UsernameException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class UserExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(UsernameException.class)
+    public ResponseEntity<?> usernameExceptionHandler(UsernameException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(EmailException.class)
+    public ResponseEntity<?> emailExceptionHandler(EmailException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(PasswordException.class)
+    public ResponseEntity<?> passwordExceptionHandler(PasswordException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+}
